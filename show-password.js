0660fb34f7f31fcd519d67ed38d1614660410f52
danKravchenko <kravchenkodan52@gmail.com>
2022-08-09 00:29:49 +0300

    changed code :)

--- show-password.js
+++ show-password.js
@@ -1,13 +1,18 @@
 let btn_show = document.getElementById("btn-show")
 let password_input = document.getElementById("password-input")
 
+let active_button = false
+
 function show_password() {
-    if (btn_show.className == "active-false") {
+
+    if (active_button == false) {
+        active_button = true
         btn_show.className = "active-true"
         password_input.setAttribute("type", "text")
     }
 
-    else if (btn_show.className == "active-true") {
+    else if (active_button == true) {
+        active_button = false
         btn_show.className = "active-false"
         password_input.setAttribute("type", "password")
     }
